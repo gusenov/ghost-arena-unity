22246efddfefd505be57de29cc5a1aab8b835f65
Аббас Гусенов <gusenov@live.ru>
2020-10-19 00:23:14 +0600

    Добавил UI для сцены с главным меню и в игровую сцену.

---
+++ Ghost Arena/Assets/Scripts/Menu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject MainPanel;
+    public GameObject DifficultyPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayGameButton_Click()
+    {
+        DifficultyPanel.SetActive(true);
+        MainPanel.SetActive(false);
+    }
+
+    public void CancelButton_Click ()
+    {
+        DifficultyPanel.SetActive(false);
+        MainPanel.SetActive(true);
+    }
+
+    public void DifficultyButton_Click(int index)
+    {
+        Globals.DifficultyLevel = index;
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitButton_Click()
+    {
+        Application.Quit();
+    }
+}
